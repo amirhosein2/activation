61f5257b6bf1bc13355dd41012e339ad3d0ff49e
amirhosein2 <sosin@debian>
2015-04-09 04:55:52 -0700

    add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,10 @@ User.create!(name:  "Example User",
              faworit_food:          "pizza",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true ,
+             activated: true,
+             activated_at: Time.zone.now)
+
 
 99.times do |n|
   name  = Faker::Name.name
@@ -14,5 +17,7 @@ User.create!(name:  "Example User",
                email: email,
                faworit_food: "pizza",
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
