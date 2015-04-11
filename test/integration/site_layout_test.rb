3b4e8025b12ec10ccd36b4981dd072bc03255e75
amirhosein2 <sosin@debian>
2015-04-10 19:04:44 -0700

    add test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,4 +10,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
+
+  test "micropost counts" do
+    before { click_link "delete", match: :first }
+    it "should be singular when count eq to 1" do
+      expect(page).to have_selector("span", text: "1 micropost")
+    end
+  end
 end
+
